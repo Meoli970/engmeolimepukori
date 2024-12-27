84b9fc739751ad16934397b282321fd97ea65d16
Meoli970 <117481561+Meoli970@users.noreply.github.com>
2024-12-28 01:27:42 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new typed(".typing",{
+    strings:["WEB DEVELOPER","WEB DESIGNER","FIGMA EXPERT","SPSS AND STATA ANALYST"],
+    typeSpeed:60,
+    BackSpeed:60,
+    loop:true
+});
